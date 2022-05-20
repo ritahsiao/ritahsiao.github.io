39f6e3aa6e9bd1f3daefa484c3fdf3bedcf92a68
蕭泳嘉 <91250519+ritahsiao@users.noreply.github.com>
2022-05-20 09:37:59 +0800

    Update food.js

--- food/food.js
+++ food/food.js
@@ -13,19 +13,25 @@ function draw() {
     // 字填充的顏色
     fill(color(0x45, 0x6F, 0xEA));
     if (choice == 0)
-        rect(50, 50, 75, 2);
+        rect(50, 50, 150, 2);
     if (choice == 1)
         rect(50, 100, 150, 2);
     if (choice == 2)
-        rect(50, 150, 200, 2);
+        rect(50, 150, 150, 2);
+    if (choice == 3)
+        rect(50, 200, 150, 2);
+    if (choice == 4)
+        rect(50, 250, 150, 2);
     text("泡麵", 50, 50);
     text("Ubereat", 50, 100);
     text("Foodpanda", 50, 150);
+    text("吃牛排", 50, 200);
+    text("吃大餐", 50, 250);
 }
 
 function mousePressed() {
     // choice = (choice+1)%3;
-    choice = int(random(3));
+    choice = int(random(5));
 }
 
 function initializeFields() {
